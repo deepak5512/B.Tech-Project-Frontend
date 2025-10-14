92e15a788f7becfab6c3c645e43cf55687c0b46d
Deepak Bhatter <120327568+deepak5512@users.noreply.github.com>
2025-10-15 05:08:13 +0530

    Multipage Website Error Fixed

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base: process.env.VITE_BASE_PATH || 'B.Tech-Project-Frontend',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
